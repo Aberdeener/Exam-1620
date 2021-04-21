57c186a329d80a75ebea6d3c66894d2c9f6ee10d
Tadhg <tadhgsmboyle@gmail.com>
2021-04-21 13:34:12 -0700

    :sparkles: Complete JS section of exam

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,79 @@
+const ALBUMS = [
+    { name: "Album 1", price: 12.99, inCart: false },
+    { name: "Album 2", price: 14.99, inCart: false },
+    { name: "Album 3", price: 9.99, inCart: false },
+    { name: "Album 4", price: 19.99, inCart: false }
+];
+
+let SHOP_ITEMS = null;
+let SHOP_ITEM_IMAGES = null;
+let CART = null;
+
+function getAlbumByName(name) {
+    for (const album of ALBUMS) {
+        if (album.name == name) {
+            return album;
+        }
+    }
+
+    return undefined;
+}
+
+function handleAlbumClick(item) {
+    clicked_album_name = item.childNodes[1].textContent;
+    album = getAlbumByName(clicked_album_name);
+
+    if (album == undefined) {
+        return;
+    }
+
+    if (album.inCart) {
+        alert('That album is already in your cart!')
+        return;
+    }
+
+    album.inCart = true;
+
+    reloadCart();
+}
+
+function reloadCart() {
+
+    CART.innerHTML = '';
+
+    for (const album of ALBUMS) {
+        if (!album.inCart) {
+            continue;
+        }
+
+        const photo_name = (album.name).replace(' ', '');
+
+        let html = `
+            <div class="cart-row">
+                <span class="cart-image"><img src="./img/${photo_name}.png"></img></span>
+                <span class="cart-name">${album.name}</span>
+                <span class="cart-price">$${album.price}</span>
+            </div>
+        `;
+
+        CART.innerHTML += html;
+    }
+}
+
 window.onload = () => {
 
-    document.querySelectorAll('.shop-item-image').forEach(element => {
-        photo_name = (element.id).replace(' ', '');
-        element.src = `./img/${photo_name}.png`;
+    SHOP_ITEMS = document.querySelectorAll('.shop-item');
+    SHOP_ITEMS.forEach(item => {
+        item.addEventListener('click', () => handleAlbumClick(item));
     });
 
+    SHOP_ITEM_IMAGES = document.querySelectorAll('.shop-item-image');
+    SHOP_ITEM_IMAGES.forEach(item => {
+        photo_name = (item.id).replace(' ', '');
+        item.src = `./img/${photo_name}.png`;
+    });
+
+    CART = document.getElementById('cart-items');
+
+    reloadCart();
 }
